2c1db2ef82c2da7e64c2ed05985f9f1c079c33da
Von Russel <v0nrussel@gmail.com>
2014-09-17 16:40:06 +0800

    fix installation of graphics magick

--- scripts/linux/install-graphicsmagick.sh
+++ scripts/linux/install-graphicsmagick.sh
@@ -8,9 +8,6 @@ sudo dpkg --configure -a
 set -e
 
 # Install GraphicsMagick
-cd /usr/local/share/
-sudo wget http://78.108.103.11/MIRROR/ftp/GraphicsMagick/1.3/GraphicsMagick-1.3.20.tar.bz2 > /dev/null
-sudo tar xjf GraphicsMagick-1.3.20.tar.bz2  > /dev/null
-cd GraphicsMagick-1.3.20
-sudo make
-sudo make install
+sudo add-apt-repository ppa:dhor/myway -y > /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get install graphicsmagick > /dev/null
